e9f83245c16b430c01d8e7a6ef244b3e6f5646b4
cooperbraun13 <cooperjbraun13@gmail.com>
2025-03-08 03:56:17 -0800

    running into issues but tired so gn

--- main.go
+++ main.go
@@ -1,3 +1,13 @@
+package main
+
+import (
+	"strings"
+	"os"
+	"bufio"
+	"fmt"
+	"os/exec"
+)
+
 // Keyboard is our standard input device (os.Stdin)
 // Each time we press the enter key, a new line is created (\n)
 // Everything written is stored in input
@@ -5,9 +15,17 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		// Read keyboard input
-		input, err := reader.ReadString("\n")
-		if err ! nil {
+		input, err := reader.ReadString('\n')
+		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
 	}
+}
+
+func execInput(input string) error {
+	// Remove newline character
+	input = strings.TrimSuffix(input, '\n')
+
+	// Prepare command to execute
+	cmd := exec.Command(input)
 }
